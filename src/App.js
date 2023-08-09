a949fba1550018cbff0562f34387bc9093c54da4
marystefi92 <maria.stefaniuk.ms@gmail.com>
2023-08-09 20:16:44 +0200

    create react  weather app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import WeatherSearch from './WeatherSearch';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch />
+    </div>
+  );
+}
+
+export default App;
