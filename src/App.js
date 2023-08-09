0f73ac1e96fb73a7760c91a6984462ce6efc4428
marystefi92 <maria.stefaniuk.ms@gmail.com>
2023-08-09 20:27:50 +0200

    added source github

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import WeatherSearch from './WeatherSearch';
-import './App.css';
+import WeatherSearch from "./WeatherSearch";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
       <WeatherSearch />
+      <p>
+        This website was coded by Mariia Stefaniuk, and is
+        <br />
+        <a href=" https://github.com/MaryStef92/weather-react" target="_blank">
+          open-sourced
+        </a>
+      </p>
     </div>
   );
 }
