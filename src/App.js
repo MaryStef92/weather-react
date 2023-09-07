f4f34740e4702a9b5b8f6a0ce83a58e6df733873
marystefi92 <maria.stefaniuk.ms@gmail.com>
2023-09-07 13:04:30 +0200

    added weather component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-        <button className=" btn btn-primary">Hello</button>
+       
+        <Weather />
         <footer>
           This website was coded by Mariia Stefaniuk, and is
-          <br />
+        { }
           <a
             href=" https://github.com/MaryStef92/weather-react"
             target="_blank"
