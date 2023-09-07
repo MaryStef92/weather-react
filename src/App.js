11e202155c26caeb501961e449bf6fe2722a945c
marystefi92 <maria.stefaniuk.ms@gmail.com>
2023-09-07 11:47:58 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import WeatherSearch from "./WeatherSearch";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSearch />
-      <p>
-        This website was coded by Mariia Stefaniuk, and is
-        <br />
-        <a href=" https://github.com/MaryStef92/weather-react" target="_blank">
-          open-sourced
-        </a>
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className=" btn btn-primary">Hello</button>
+        <footer>
+          This website was coded by Mariia Stefaniuk, and is
+          <br />
+          <a
+            href=" https://github.com/MaryStef92/weather-react"
+            target="_blank"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
